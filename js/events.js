c48be71691e184f540cb4825795270d12f91ac00
Manas Khurana <manas.khurana20@gmail.com>
2019-01-27 21:50:17 +0530

    add gallery

--- js/events.js
+++ js/events.js
@@ -5,10 +5,12 @@ var eventList = [{
     info: 'The first ever Hackathon of GU',
     folder: 'dex1', // the folder mentioned here will have all photos.
     // The folder should be present in './events/' directory
+    numberOfPhotos: 5
 }, {
     name: 'Dexterix Hackathon 2.0',
     date: new Date(2019, 11, 10, 8, 0, 0, 0),
     venue: 'C - Block, Galgotias Univesity',
     info: 'The second best thing to have happened this year (the first best being the release of Avengers: Endgame)',
-    folder: 'dex2',
+    folder: '',
+    numberOfPhotos: 0
 }];
