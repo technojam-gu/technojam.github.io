2fc3b475ba62a4198cf4da023aa947fdbd2cc068
Manas Khurana <manas.khurana20@gmail.com>
2019-01-28 00:56:14 +0530

    compress icons

--- js/events.js
+++ js/events.js
@@ -1,7 +1,7 @@
 var eventList = [{
     name: 'Dexterix Hackathon',
     date: new Date(2018, 11, 10, 8, 0, 0, 0),
-    venue: 'C - Block, Galgotias Univesity',
+    venue: 'C-Block, Galgotias Univesity',
     info: 'The first ever Hackathon of GU',
     folder: 'dex1', // the folder mentioned here will have all photos.
     // The folder should be present in './events/' directory
@@ -9,7 +9,7 @@ var eventList = [{
 }, {
     name: 'Dexterix Hackathon 2.0',
     date: new Date(2019, 11, 10, 8, 0, 0, 0),
-    venue: 'C - Block, Galgotias Univesity',
+    venue: 'C-Block, Galgotias Univesity',
     info: 'The second best thing to have happened this year (the first best being the release of Avengers: Endgame)',
     folder: '',
     numberOfPhotos: 0
