6ab805b7fd002eef50a83810e49d91e2ca728e71
Manas Khurana <manas.khurana20@gmail.com>
2019-01-28 20:07:14 +0530

    update serviceworker; attempt 404 fix

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,14 +1,23 @@
-var CACHE_NAME = 'tj-v1.0';
+var CACHE_NAME = 'tj-cache-v1.0';
 var urlsToCache = [
-    '/index.html',
+    '/',
     '/css/global.css',
     '/js/global.js',
+    '/images/logo.png',
+    '/images/social/facebook.png',
+    '/images/social/instagram.png',
+    '/images/social/mail.png',
+    '/images/index/logo.png',
     '/team.html',
     '/js/team.js',
+    '/images/team/github.png',
+    '/images/team/linkedin.png',
     '/events.html',
     '/gallery.html',
+    '/images/gallery/close.png',
+    '/images/gallery/next.png',
+    '/events.html'
 ];
-
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -17,15 +26,31 @@ self.addEventListener('install', function(event) {
         })
     );
 });
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            if (response) {
-                return response;
-            } else {
-                return fetch(event.request);
-            }
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+        return Promise.all(
+            cacheNames.filter(function (cacheName) {
+                return cacheName != CACHE_NAME;
+            }).map(function (cacheName) {
+                return caches.delete(cacheName);
+            })
+        );
         })
     );
+});
+
+self.addEventListener('fetch', function(event) {
+    try {
+        event.respondWith(
+            caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                } else {
+                    return fetch(event.request);
+                }
+            })
+        );
+    } catch (err) {}
 });
