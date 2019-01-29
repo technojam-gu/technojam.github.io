0c497ac22a71cba3b8cfeb769201562f79314f3b
Prakhar-Srivastava <18552460+Prakhar-Srivastava@users.noreply.github.com>
2019-01-29 17:30:03 +0530

    New Index Page

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from flask import Flask,render_template,url_for
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
--- js/global.js
+++ js/global.js
@@ -1,6 +1,5 @@
 const gebi=id=>document.getElementById(id)
 function navigator_menu(e){
 	gebi('navs').classList.toggle('show')
-	console.log('I\'m clicked')
 }
 gebi('logo').addEventListener('click',navigator_menu)
